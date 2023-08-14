d518a66d98790701bdc360b925d533310f0015fe
Hanief <hanief@gmail.com>
2023-08-14 21:36:09 +0700

    add download icon

--- src/posts/Brosur.tsx
+++ src/posts/Brosur.tsx
@@ -4,7 +4,9 @@ export default function Brosur() {
   return (
     <div id="brosur">
       <div className="d-grid gap-2 mb-2">
-        <Link target="_blank" className="btn btn-primary btn-lg mb-2" href="brosur_psb_2024.pdf"  download="brosur_psb_2024.pdf">Download Brosur</Link>
+        <Link target="_blank" className="btn btn-primary btn-lg mb-2" href="brosur_psb_2024.pdf" download="brosur_psb_2024.pdf">
+          <i className="bi bi-download me-1"></i>Download Brosur
+        </Link>
       </div>
       <embed src="brosur_psb_2024.pdf" width="100%" height="1200px" />
     </div>
