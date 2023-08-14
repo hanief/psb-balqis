dc76bc090344ff9775de9c5c8d6bd5feac36f699
Hanief <hanief@gmail.com>
2023-08-14 19:22:56 +0700

    Add more slides

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,8 +4,12 @@ import { Carousel } from 'react-responsive-carousel'
 export default function CarouselComponent() {  
   return (
     <Carousel autoPlay={true} showArrows={true} showStatus={false} infiniteLoop={true}>
-      <Image className="bd-placeholder-img" src="/slide2.jpg" width={768} height={431} alt="Santri putri"></Image>
-      <Image className="bd-placeholder-img" src="/slide1.jpg" width={768} height={431} alt="Santri putra"></Image>
+      <Image className="bd-placeholder-img" src="/slide1.jpg" width={768} height={431} alt="Santri"></Image>
+      <Image className="bd-placeholder-img" src="/slide2.jpg" width={768} height={431} alt="Santri"></Image>
+      <Image className="bd-placeholder-img" src="/slide3.jpg" width={768} height={431} alt="Santri"></Image>
+      <Image className="bd-placeholder-img" src="/slide4.jpg" width={768} height={431} alt="Santri"></Image>
+      <Image className="bd-placeholder-img" src="/slide5.jpg" width={768} height={431} alt="Santri"></Image>
+      <Image className="bd-placeholder-img" src="/slide6.jpg" width={768} height={431} alt="Santri"></Image>
     </Carousel>
   )
 }
