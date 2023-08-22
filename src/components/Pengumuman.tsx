7f427a8db0b3657153cacb78e1944a147f53537f
Hanief <hanief@gmail.com>
2023-08-22 08:25:23 +0700

    fix pengumuman

--- src/components/Pengumuman.tsx
+++ src/components/Pengumuman.tsx
@@ -1,19 +1,11 @@
 import { Button } from "reactstrap";
 
-export default function Pengumuman({onSuccess}) {
+export default function Pengumuman() {
   return (
     <>
       <h1>Pengumuman</h1>
-
-      <p>Selamat, anda telah</p>
-      <h1 className="text-success">DITERIMA</h1>
-      <p>di Balqis Jogja.</p>
-
-      <p>Silakan mendaftarkan ulang</p>
-
-      <Button color='primary' className='me-1' onClick={onSuccess}>
-        Lanjut
-      </Button>
+      <p>Belum ada pengumuman kelulusan saat ini</p>
+      <p>Anda dapat melihat pengumuman setelah tes berakhir dan panitia menentukan kelulusan.</p>
     </>
   )
 }
