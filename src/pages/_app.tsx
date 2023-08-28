656db76cc6867c3653348fb26b284402114136b6
Hanief <hanief@gmail.com>
2023-08-28 17:05:45 +0700

    add bugsnag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,9 +9,21 @@ import { Lato, Heebo } from 'next/font/google'
 import Layout from '@/components/Layout'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import {Database} from '@/lib/database.types'
 import { Analytics } from '@vercel/analytics/react'
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react'
+
+Bugsnag.start({
+  apiKey: process.env.NEXT_PUBLIC_BUGSNAG_API_KEY,
+  plugins: [new BugsnagPluginReact()],
+  appVersion: process.env.NEXT_PUBLIC_APP_VERSION,
+  releaseStage: process.env.NEXT_PUBLIC_APP_ENVIRONMENT
+})
+
+const ErrorBoundary = Bugsnag.getPlugin('react')
+  .createErrorBoundary(React)
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] })
 const heebo = Heebo({ subsets: ['latin'], weight: ['400', '700'] })
@@ -23,19 +35,21 @@ export default function MyApp({
   const [supabaseClient] = useState(() => createBrowserSupabaseClient<Database>())
   
   return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={initialSession}
-    >
-      <style jsx global>{`
-        body {
-          font-family: ${lato.style.fontFamily};
-        }
-      `}</style>
-      <Layout>
-        <Component {...pageProps} />
-        <Analytics />
-      </Layout>
-    </SessionContextProvider>
+    <ErrorBoundary>
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={initialSession}
+      >
+        <style jsx global>{`
+          body {
+            font-family: ${lato.style.fontFamily};
+          }
+        `}</style>
+          <Layout>
+            <Component {...pageProps} />
+            <Analytics />
+          </Layout>
+      </SessionContextProvider>
+    </ErrorBoundary>
   )
 }
