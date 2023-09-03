0ac0ee2a833f29578a4508bbec95cc17b643a323
Hanief <hanief@gmail.com>
2023-09-03 13:37:00 +0700

    fix margin table

--- src/components/ExpandedRow.tsx
+++ src/components/ExpandedRow.tsx
@@ -1,20 +1,27 @@
 import { columns } from "@/data/columns"
 import { convertToTitleCase, formatDatumWithWilayahNames } from "@/utils"
+import { Col, Row, Table } from "reactstrap"
 
-export default function ExpandedComponent({data}) {
+export default function ExpandedRow({data}) {
   const formattedData = formatDatumWithWilayahNames(data, 1)
 
   return (
-    <table className="table table-striped m-2">
-      <tbody>
-      {columns.map(column => (
-        <tr key={column}>
-          <th scope="row">{convertToTitleCase(column)}</th>
-          <td>:</td>
-          <td>{formattedData[column]}</td>
-        </tr>
-      ))}
-      </tbody>
-    </table>
+    <Row>
+      <Col>
+        <Table className="table-striped">
+          <tbody>
+          {columns.map(column => (
+            <tr key={column}>
+              <th scope="row">{convertToTitleCase(column)}</th>
+              <td>:</td>
+              <td>{formattedData[column]}</td>
+            </tr>
+          ))}
+          </tbody>
+        </Table>
+      </Col>
+      <Col className="flex-grow-1">
+      </Col>
+    </Row>
   )
 }
