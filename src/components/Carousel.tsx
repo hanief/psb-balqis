532f2c23de9d8e741d8be664eaece790b86f82dd
Hanief <hanief@gmail.com>
2023-09-07 13:47:09 +0700

    fix url

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,13 +3,14 @@ import Image from "next/image"
 import { Carousel } from 'react-responsive-carousel'
 
 export default function CarouselComponent() {
-  const { slidesURL } = useFileContents()
+  const { slides } = useFileContents()
 
   return (
     <Carousel autoPlay={true} showArrows={true} showStatus={false} infiniteLoop={true} showThumbs={false}>
-      {slidesURL?.map(slide => {
+      {slides?.map(slide => {
+        const url = `https://cfubtesizbmwfhtuzzav.supabase.co/storage/v1/object/public/contents/slide/${slide.name}`
         return (
-          <Image key={slide} className="bd-placeholder-img rounded" src={slide} width={768} height={431} alt="Santri"></Image>
+          <Image key={slide.name} className="bd-placeholder-img rounded" src={url} width={768} height={431} alt="Santri Balqis Jogja"></Image>
         )
       })}
     </Carousel>
