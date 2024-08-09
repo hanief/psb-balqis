3e281792744d364b6468a937be7109d152d95985
Hanief <hanief@gmail.com>
2024-08-09 22:18:09 +0700

    fix carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -7,17 +7,17 @@ export default function CarouselComponent() {
   const { slides } = useFileContents()
 
   return (
-    <Container className="m-1 p-1" fluid>
-      <Carousel autoPlay={true} showArrows={true} showStatus={false} infiniteLoop={true} showThumbs={false}>
-        {slides?.map(slide => 
+    <Container fluid>
+      <Carousel className="my-3" autoPlay={true} showArrows={true} showStatus={false} infiniteLoop={true} showThumbs={false}>
+        {slides?.map(slide => (
           <Image 
             key={slide.id} 
             className="bd-placeholder-img rounded" 
             src={slide.url} 
-            width={768} 
-            height={431}
+            width={750} 
+            height={400}
             alt="Santri Balqis Jogja" />
-        )}
+        ))}
       </Carousel>
     </Container>
   )
